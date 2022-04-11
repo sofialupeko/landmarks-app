676efa92bca231341870940663c0dba64fb7495b
Sofia Lupeko <sonya.lupeko@mail.ru>
2022-04-11 18:02:07 +0300

    Project creation

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Denis Valshchikov on 11.04.2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(
+                    Color.white,
+                    lineWidth: 4
+                )
+            )
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
